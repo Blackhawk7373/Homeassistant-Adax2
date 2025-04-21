626cd64fdf176c8c6b6652706851768f138ae8b6
Blackhawk7373 <76883372+Blackhawk7373@users.noreply.github.com>
2025-04-21 22:00:18 +0200

    Update --init--.py

--- custom_components/adax2/--init--.py
+++ custom_components/adax2/--init--.py
@@ -1,1 +1,7 @@
+"""Adax integration for Home Assistant."""
+DOMAIN = "adax2"
 
+def setup(hass, config):
+    """Legacy setup; om du inte implementerar config_flow."""
+    hass.data[DOMAIN] = {}
+    return True
